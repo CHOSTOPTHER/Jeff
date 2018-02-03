2876540a50f472ede6920a23caca6c41fd671784
jeff <784995682@qq.com>
2018-02-03 15:07:39 +0800

    环境搭建

---
+++ OpenGL环境搭建/Build/Intermediates/OpenGL环境搭建.build/Debug/OpenGL环境搭建.build/Objects-normal/x86_64/mian.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/lizhenhua/Desktop/openGL/OpenGL环境搭建/OpenGL环境搭建/mian.cpp \
+  /Users/lizhenhua/Desktop/openGL/OpenGL环境搭建/OpenGL环境搭建/include/GLShaderManager.h \
+  /Users/lizhenhua/Desktop/openGL/OpenGL环境搭建/OpenGL环境搭建/include/GL/glew.h \
+  /Users/lizhenhua/Desktop/openGL/OpenGL环境搭建/OpenGL环境搭建/include/GLTools.h \
+  /Users/lizhenhua/Desktop/openGL/OpenGL环境搭建/OpenGL环境搭建/include/math3d.h \
+  /Users/lizhenhua/Desktop/openGL/OpenGL环境搭建/OpenGL环境搭建/include/GLBatch.h \
+  /Users/lizhenhua/Desktop/openGL/OpenGL环境搭建/OpenGL环境搭建/include/GLBatchBase.h \
+  /Users/lizhenhua/Desktop/openGL/OpenGL环境搭建/OpenGL环境搭建/include/GLTriangleBatch.h
